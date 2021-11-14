97a3d2301dc89e07b8f1336e4f80d142bfabc6a5
ritarded <ritalino7@hotmail.com>
2021-11-14 22:37:11 +0000

    update to coisa das encomendas

--- 1aFase/conhecimento.pl
+++ 1aFase/conhecimento.pl
@@ -8,16 +8,15 @@ estafeta(1, 'Ana Fernandes', [2,4,3], [1,4]).
 cliente(1, 'Fernando Mendes', [1,2,4]).
 
 
-%encomenda(IdEncomenda, Data, Peso, Volume, IdEstafeta, Valor, idTransporte, idcliente, concelho, estado, prazoEntrega)
+%encomenda(IdEncomenda, (IdEstafeta, idTransporte, idcliente), (Data, prazoEntrega), (Peso, Volume, Valor, Estado), concelho)
 % estado : 1 - entregue
 %          0 - pendente
 %          -1 - cancelada
 
-encomenda(1, '19/01/2021', 5, 200, 1, 40, 1, 1, 'Braga', 1, 72).
+encomenda(1, (1, 2, 4), ('19/01/2021', 72), (16, 2, 25, 0), 'Braga').
 
 
 %transporte(idTransporte,Nome, PesoMaximo, VelocidadeMedia, nivelEcologico)
 transporte(1, 'bicicleta', 5, 10, 3).
 transporte(2, 'mota', 20, 35, 2).
 transporte(3, 'carro', 100, 25, 1).
-
