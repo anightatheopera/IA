fe4e161c481b4c12afc377f491a9215ce0c9ecf6
ritarded <ritalino7@hotmail.com>
2021-11-15 18:46:23 +0000

    fixed primeiro predicado para ser mais geral e nao so para funcionar com bicicletas yuh

--- 1aFase/predicados.pl
+++ 1aFase/predicados.pl
@@ -1,5 +1,6 @@
 :- consult([conhecimento]).
 
+% funções auxiliares -----------------------------
 
 % [(A, Number)] -> (A, Number)
 max_on_snd([H], H).
@@ -8,16 +9,22 @@ max_on_snd([(A0,N0)|T], Res) :-
     Max is max(N0, N1),
     (Max =:= N0 -> Res = (A0,N0); Res = (A1,N1)).
 
+% ------------------------------------------------
+
 % predicado 1
-ecotrans(Res) :- 
-    transporte(IdBike, bicicleta, _, _, _),
+maisecologico(Res) :-
+    % encontrar todos os transportes
+    findall((EcoRate,IdT),
+          transporte(IdT, _, _, _, EcoRate),
+          L0),
+    sort(0, @>=, L0, [(_,IdT)|_]),
     findall((Id, N)
         , (estafeta(Id, _, _)
-            , findall(_, encomenda(_, (Id, IdBike, _), _, _, _), Es)
+            , findall(_, encomenda(_, (Id, IdT, _), _, _, _), Es)
             , length(Es, N))
         , L),
     max_on_snd(L, (Res, _)).
-
+    
 % predicado 2
 estcliente(IdCliente, Res) :-
     findall((IdEst,IdEnc), encomenda(IdEnc,(IdEst,_,IdCliente), _, _, _), Res).
