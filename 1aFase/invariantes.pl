7ecf46b29aa6188bd4b07fa2bbbd2b1d28cbac65
MayorX500 <angola2001@gmail.com>
2021-12-02 12:33:22 +0000

    fixed some invariantes

--- 1aFase/invariantes.pl
+++ 1aFase/invariantes.pl
@@ -6,23 +6,23 @@
 % ------------- ESTAFETA -------------- --- -- -- - - - - 
 
 % Não permitir a INSERÇÃO de conhecimento repetido pelo id 
-+estafeta( IdE, _, _ ) :: (findall( J, estafeta( IdE, J, _ ), R ),comprimento(R, 1 )).
++estafeta( IdE, _, _ ) :: (findall( J, estafeta( IdE, J, _ ), R ),length(R, 0 )).
 
-% Não permitir REMOVER um IDEstafeta já associado a uma encomenda
+% Permitir REMOVER um IDEstafeta já associado a uma encomenda
 -estafeta( IdE, _, _ ) :: (findall( J, encomenda( J, ( IdE, _, _ ), _, _, _), R )).
 
 % ------------- CLIENTE ---------------- --- -- -- - - - - 
 
 % Não permitir a INSERÇÃO de conhecimento repetido pelo id 
-+cliente( IdC, _, _ ) :: (findall( J, cliente( IdC, J, _ ), R ),comprimento( R, 1 )).
++cliente( IdC, _, _ ) :: (findall( J, cliente( IdC, J, _ ), R ),comprimento( R, 0 )).
 
-% Não permitir REMOVER um IDCliente já associado a uma encomenda
+% Permitir REMOVER um IDCliente já associado a uma encomenda
 -cliente( IdE, _, _ ) :: (findall( IdC, encomenda( IdC, ( _, _, IdE ), _, _, _), R )).
 
 % ------------- ENCOMENDA -------------- --- -- -- - - - - 
 
 % Não permitir a INSERÇÃO de conhecimento repetido pelo id 
-+encomenda( Id, _, _, _, _ ) :: (findall( IdR, encomenda( Id, _, _, _, IdR ), R ), comprimento( R, 1)).
++encomenda( Id, _, _, _, _ ) :: (findall( IdR, encomenda( Id, _, _, _, IdR ), R ), comprimento( R, 0)).
 
-% Não permitir a REMOÇÃO de conhecimento repetido pelo id
+% Permitir a REMOÇÃO de conhecimento repetido pelo id
 -encomenda( Id, _, _, _, _ ) :: (findall( IdF, encomenda( Id, _, _, _, F ), R)). 
